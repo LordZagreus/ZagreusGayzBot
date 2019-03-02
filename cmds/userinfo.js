eda27dc56ea0713ee5c1f831f03b93d5750668a0
Luzhou Zhang <lzhang1337@gmail.com>
2019-03-01 18:32:23 -0800

    can i uh get some GIT COMMIT ORIGIN

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -3,14 +3,13 @@ module.exports.run = async (bot, message, args) => {
   let member = message.mentions.members.first() || message.member
   	user = member.user
   let embed = new Discord.RichEmbed()
-    .setAuthor(`User Information for ${message.author.username}`)
-    .setThumbnail(message.author.displayAvatarURL)
+    .setAuthor(`User Information for ${user.username}`)
+    .setThumbnail(user.displayAvatarURL)
     .setAuthor(`User Information for ${user.username}`)
     .setThumbnail(user.displayAvatarURL)
     .setColor("#0073CF")
     .addField("Full Username", user.tag)
-    .addField("Discord ID", message.author.id)
-    .addField("Account Creation Date", message.author.createdAt)
+    .addField("Discord ID", user.id)
     .addField("Roles", member.roles.map(r => `${r}`).join(' | '), true)
     //haha idk why join date always returns undefined
     .addField("Latest Server Join Date", user.joinedAt)

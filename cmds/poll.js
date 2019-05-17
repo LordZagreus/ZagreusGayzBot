d13bb907a2e2413d072a56c4a98c375b7877ec40
LordZagreus <lzhang1337@gmail.com>
2019-05-16 19:38:35 -0700

    poll finally takes custom times

--- cmds/poll.js
+++ cmds/poll.js
@@ -2,16 +2,22 @@ const agree = "✅"
 const disagree = "⛔"
 
 module.exports.run = async (bot, message, args) => {
-	var arglength = args.length
-  var voteargs = message.content
-  var votemsg = voteargs.slice(4+arglength)
-  
-  let msg = await message.channel.send(votemsg)
+  var votemsg1 = message.content
+  let votemsg = votemsg1.slice(5)
+  var stringarray=votemsg.split(" ")
+
+  votetime=stringarray[1]*1000
+
+  //i hate regular expressions
+  if (isNaN(votetime)){
+  	return message.channel.send("error caught in votetime. poll syntax is !poll [time in seconds] [text]. please format correctly or i refuse to function properly.")
+  }
+  let finalcut=votemsg.slice(stringarray[1].length+1)
+  let msg = await message.channel.send(finalcut)
   await msg.react(agree)
   await msg.react(disagree)
-
-  const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === agree || reaction.emoji.name === disagree, {time: 10000})
-  message.channel.send(`Voting complete\n${agree}: ${reactions.get(agree).count-1}\n${disagree}: ${reactions.get(disagree).count-1}`)
+  const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === agree || reaction.emoji.name === disagree, {time: votetime})
+  message.channel.send(`Voting complete for:${finalcut}\n${agree}: ${reactions.get(agree).count-1}\n${disagree}: ${reactions.get(disagree).count-1}`)
 
 }
 

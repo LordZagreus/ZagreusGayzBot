b7e74b407821da57e7925396be9b2d7373bdd34b
LordZagreus <lzhang1337@gmail.com>
2019-06-14 09:16:57 -0700

    merging for reasons

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -5,14 +5,20 @@ module.exports.run = async (bot, message, args) => {
   let embed = new Discord.RichEmbed()
     .setAuthor(`User Information for ${user.username}`)
     .setThumbnail(user.displayAvatarURL)
+<<<<<<< Updated upstream
     .setAuthor(`User Information for ${user.username}`)
     .setThumbnail(user.displayAvatarURL)
+=======
+>>>>>>> Stashed changes
     .setColor("#0073CF")
     .addField("Full Username", user.tag)
     .addField("Discord ID", user.id)
     .addField("Roles", member.roles.map(r => `${r}`).join(' | '), true)
     .addField("Latest Server Join Date", user.joinedAt)
+<<<<<<< Updated upstream
     //above always returns undefined for some reason
+=======
+>>>>>>> Stashed changes
     .addField("Account Creation Date", user.createdAt)
 
   message.channel.send({embed: embed})
